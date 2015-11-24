14ecaeb0cf730bdb43a5f200beb7b35a07b7ce34
debu <rony.debu@gmail.com>
2015-11-24 16:03:23 +0530

    First Commit for Excel Addin

---
+++ ExcelAddIn/RibbonFetch.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ExcelAddIn
+{
+    partial class RibbonFetch : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public RibbonFetch()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.grpData = this.Factory.CreateRibbonGroup();
+            this.btnFetch = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.grpData.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.grpData);
+            this.tab1.Label = "Fetch-Sample";
+            this.tab1.Name = "tab1";
+            // 
+            // grpData
+            // 
+            this.grpData.Items.Add(this.btnFetch);
+            this.grpData.Label = "LoadData";
+            this.grpData.Name = "grpData";
+            // 
+            // btnFetch
+            // 
+            this.btnFetch.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btnFetch.Image = global::ExcelAddIn.Properties.Resources.dwnload;
+            this.btnFetch.ImageName = "download";
+            this.btnFetch.Label = "Fetch Data";
+            this.btnFetch.Name = "btnFetch";
+            this.btnFetch.ShowImage = true;
+            this.btnFetch.SuperTip = "Click to Fetch Data";
+            this.btnFetch.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnFetch_Click);
+            // 
+            // RibbonFetch
+            // 
+            this.Name = "RibbonFetch";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.RibbonFetch_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.grpData.ResumeLayout(false);
+            this.grpData.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup grpData;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnFetch;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal RibbonFetch RibbonFetch
+        {
+            get { return this.GetRibbon<RibbonFetch>(); }
+        }
+    }
+}
---
+++ ExcelAddIn/RibbonFetch.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Net;
+using System.Xml.Linq;
+using System.Data;
+
+namespace ExcelAddIn
+{
+    public partial class RibbonFetch
+    {
+        Excel.Range newFirstRow;
+        Excel.Range firstRow;
+        Excel.Worksheet activeWorksheet;
+        private void RibbonFetch_Load(object sender, RibbonUIEventArgs e)
+        {
+            
+        }
+
+        private void btnFetch_Click(object sender, RibbonControlEventArgs e)
+        {
+            activeWorksheet = ((Excel.Worksheet)Globals.ThisAddIn.Application.ActiveSheet);
+            firstRow = activeWorksheet.get_Range("A1");
+            //firstRow.EntireRow.Insert(Excel.XlInsertShiftDirection.xlShiftDown);
+            newFirstRow = activeWorksheet.get_Range("A1");
+
+            try
+            {
+                firstRow.Value2 = "Fetching Data Please Wait...";
+                string url = "http://localhost:5000/sample/getdata";
+                WebClient wc = new WebClient();
+                wc.DownloadStringCompleted += HttpsCompleted;
+                wc.DownloadStringAsync(new Uri(url));
+
+                
+
+            }
+            catch(Exception ex)
+            {
+                firstRow.Value2 = "An Error occured while accessing the service";
+            }
+            
+        }
+        private void HttpsCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            if (e.Error == null)
+            {
+                string response = e.Result;
+                DataTable xmlData = Newtonsoft.Json.JsonConvert.DeserializeObject<DataTable>(response);
+                //DataTable dtObj = new DataTable("MyExcel");
+                foreach (DataColumn cols in xmlData.Columns)
+                {
+                    newFirstRow.Value2 = cols.ColumnName;
+                    newFirstRow = newFirstRow.Next;
+                }
+                newFirstRow = activeWorksheet.Cells[firstRow.Row + 1, firstRow.Column];
+                object[] data = new object[xmlData.Columns.Count];
+                foreach (DataRow dr in xmlData.Rows)
+                {
+                    data = new object[xmlData.Columns.Count];
+                    data = dr.ItemArray;
+
+                    //= (Excel.Range)activeWorksheet.Cells[newFirstRow.Row, firstRow.Column]; ;
+                    Excel.Range newRangeData = newFirstRow.get_Resize(1, xmlData.Columns.Count);
+
+                    newRangeData.set_Value(Type.Missing, data);
+                    newFirstRow = activeWorksheet.Cells[newFirstRow.Row + 1, firstRow.Column];
+                }
+                firstRow.get_Offset(1, 1).get_Resize(xmlData.Rows.Count, xmlData.Columns.Count).Style = "Currency";
+            }
+            else
+            {
+                firstRow.Value2 = "An Error occured while accessing the service";
+            }
+        }
+    }
+}
